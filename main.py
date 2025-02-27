b69373278739d29da589959478c373c337d08138
kelei <hiwork024@gmail.com>
2025-02-28 00:10:59 +0800

    init

--- main.py
+++ main.py
@@ -373,6 +373,8 @@ if __name__ == "__main__":
             print("Docker 登录成功，继续执行后续操作...")
             docker_compose_up()
             run_command(f"chown -R www:www /www/docker")
+            run_command(f"chmod 755 /www/docker")
+            run_command(f"chmod 755 -R /www/docker/data/ftp")
             print("请手动以下命令：")
             print("newgrp docker")
             print("sudo systemctl restart docker")
